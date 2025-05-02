5c1d05f022627ba1f5f45348edaaf2a269edcdba
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-02 11:32:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './global.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode><App/></React.StrictMode>,
   document.getElementById('root')
 );
