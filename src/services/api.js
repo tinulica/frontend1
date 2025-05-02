dbbedf7d35676d8e85cc44809d4ee5523085b29d
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-02 11:43:21 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
+// src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'https://backend11-75yc.onrender.com',
+  withCredentials: true,
 });
 
 export default api;
