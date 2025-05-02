257fc94cfedeaa588e2a0f081bdba52f660eb55c
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-02 11:14:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import './global.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
