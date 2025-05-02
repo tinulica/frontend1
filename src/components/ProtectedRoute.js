8a230f374f739c27696b7713748265871ccc8a55
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-02 12:11:12 +0300

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/login" />;
+  const { user } = useContext(AuthContext);
+  return user ? children : <Navigate to="/login" replace />;
 }
