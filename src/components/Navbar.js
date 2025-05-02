5dbee1c76101cf064942762cbe1c877665bb0d09
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-02 13:05:04 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import './Navbar.css';
 
 export default function Navbar() {
   const { user, logout } = useContext(AuthContext);
