81dfd866ba133e2f75239462dac3cf56c429f54e
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-02 10:41:45 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,12 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  headers: { 'Content-Type': 'application/json' }
 });
 
 export default api;
