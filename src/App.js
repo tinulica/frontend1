2c2268ebb64013acd0dd96146d48e2958c459a85
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-02 11:18:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home          from './components/Home';
-import Register      from './components/Register';
-import Login         from './components/Login';
-import Dashboard     from './components/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Register from './components/Register';
+import Login    from './components/Login';
+import Home     from './components/Home';
 
-export default function App() {
+function App() {
+  const token = localStorage.getItem('token');
+
   return (
-    <Routes>
-      <Route path="/"          element={<Home />} />
-      <Route path="/register"  element={<Register />} />
-      <Route path="/login"     element={<Login />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      {/* add more protected routes here */}
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ token ? <Home/> : <Navigate to="/login" /> } />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login"    element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
