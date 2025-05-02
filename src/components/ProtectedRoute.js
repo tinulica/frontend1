d32e9fb4501fa7b1384e4088dae2474c79bf2028
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-02 11:33:05 +0300

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { getAuth } from '../utils/auth';
 
 export default function ProtectedRoute({ children }) {
-  const auth = getAuth();
-  return auth ? children : <Navigate to="/login" replace />;
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login" />;
 }
