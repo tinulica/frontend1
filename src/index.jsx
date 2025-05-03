6f0eeae3965b41be57fb0e1086aa8b7b4c3e14d0
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-03 22:37:15 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import App from './App';
+import './global.css';
 
 ReactDOM.render(
   <BrowserRouter>
