90ba74962fc66d8c99a6f4051db30b2b2b56989a
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-04 23:59:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import App from './App';
-import './index.css';
+import './global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
