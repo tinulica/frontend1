2d8d77aa3a71445c6121c993c48499e128024c36
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-05 00:01:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+// index.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import App from './App';
-import './global.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </AuthProvider>
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
