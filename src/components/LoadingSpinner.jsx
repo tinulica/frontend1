8bfc26bae415705bda3a237b8bc4f5ee5b4c5499
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-11 13:27:47 +0300

    Create LoadingSpinner.jsx

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+// src/components/LoadingSpinner.jsx
+import React from 'react';
+import './LoadingSpinner.css';
+
+export default function LoadingSpinner() {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner" />
+    </div>
+  );
+}
