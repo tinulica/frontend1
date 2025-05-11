33057dfd97636f30d56495ce46eb119953927e93
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-05-11 13:58:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import ErrorBoundary from './components/ErrorBoundary'
 import App from './App'
 import './global.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
-  <ErrorBoundary>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </ErrorBoundary>
 )
